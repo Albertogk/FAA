72299686c80586719cf29f8203f8b6b81773d145
albertogk <agk1esoc@gmail.com>
2020-10-05 19:59:00 +0200

    Semana 1 p1

--- practica1/EstrategiaParticionado.py
+++ practica1/EstrategiaParticionado.py
@@ -8,6 +8,9 @@ class Particion():
         self.indicesTrain = []
         self.indicesTest = []
 
+    def __str__(self):
+        return "Test: " + str(self.indicesTest) + "\nTrain: " + str(self.indicesTrain)
+
 
 #####################################################################################################
 
@@ -18,7 +21,6 @@ class EstrategiaParticionado:
     # Atributos: deben rellenarse adecuadamente para cada estrategia concreta. Se pasan en el constructor
 
     @abstractmethod
-    # TODO: esta funcion deben ser implementadas en cada estrategia concreta
     def creaParticiones(self, n_datos, seed=None):
         pass
 
@@ -34,17 +36,16 @@ class ValidacionSimple(EstrategiaParticionado):
 
     # Crea particiones segun el metodo tradicional de division de los datos segun el porcentaje deseado y el número de ejecuciones deseado
     # Devuelve una lista de particiones (clase Particion)
-    # TODO: implementar
     def creaParticiones(self, n_datos, seed=None):
         random.seed(seed)
         ids = list(range(0, n_datos))
         n_test = round(n_datos*self.porcentaje_test)
-        random.shuffle(ids)
         particiones = []
 
         for i in range(0, self.n_ejecuciones):
+            random.shuffle(ids)
             particion = Particion()
-            particion.indicesTest = ids[0:n_test-1]
+            particion.indicesTest = ids[0:n_test]
             particion.indicesTrain = ids[n_test:]
             particiones.append(particion)
 
@@ -54,10 +55,27 @@ class ValidacionSimple(EstrategiaParticionado):
 #####################################################################################################
 class ValidacionCruzada(EstrategiaParticionado):
 
+    def __init__(self, k):
+        self.k = k
+
     # Crea particiones segun el metodo de validacion cruzada.
     # El conjunto de entrenamiento se crea con las nfolds-1 particiones y el de test con la particion restante
     # Esta funcion devuelve una lista de particiones (clase Particion)
-    # TODO: implementar
     def creaParticiones(self, n_datos, seed=None):
+        datos_por_fold = n_datos//self.k
         random.seed(seed)
-        pass
+        ids = list(range(0, n_datos))
+        random.shuffle(ids)
+        particiones = []
+
+        for i in range(0, self.k):
+            particion = Particion()
+            particion.indicesTest = ids[i*datos_por_fold: (i+1)*datos_por_fold]
+            if i != 0:
+                particion.indicesTrain += ids[0: i*datos_por_fold]
+
+            particion.indicesTrain += ids[(i+1)*datos_por_fold:]
+
+            particiones.append(particion)
+
+        return particiones
